322edb6ee430f1c39cc233a9c703ee4c1f480137
RJKKK <shengunkun@gmail.com>
2020-09-08 19:50:23 +0800

    initail

--- examples/main.js
+++ examples/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import TagTextarea from '../packages/index'
+Vue.use(TagTextarea)
 
 new Vue({
   render: h => h(App),
